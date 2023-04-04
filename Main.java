888f3e09112e8948c8b6b8aac394bebe70646042
ethanh1107 <129885069+ethanh1107@users.noreply.github.com>
2023-04-04 10:08:04 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print(" Enter a name: " );
+    String name = input.next();
+    System.out.print(" Enter an activity: ");
+    String activity = input.next();
+    System.out.println( name + " keep practicing. Never let someone tell you you're too small or too slow for " + activity + ".");
+  }
+} //"Keep practicing. Never let someone tell you you're too small or too slow." 
